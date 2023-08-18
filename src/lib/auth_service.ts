160a0e12ff70361d1c5523527e20a43ae091e963
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-08-18 16:17:51 +0200

    add initial API test

--- src/lib/auth_service.ts
+++ src/lib/auth_service.ts
@@ -1,5 +1,6 @@
 import { PUBLIC_API_HOST } from '$env/static/public';
 import { user } from '$lib/stores';
+import { goto } from '$app/navigation';
 
 export const login = async (username: string, password: string): Promise<{ status: number, detail?: string}> => {
 	const form = new FormData();
@@ -30,4 +31,53 @@ export const login = async (username: string, password: string): Promise<{ statu
 	const detail = (await res.json()).detail;
 
  	return { status, detail };
+}
+
+export const refreshAccessToken = async (refresh: string) => {
+	const res = await fetch(`${PUBLIC_API_HOST}/api/token/refresh`, {
+		method: 'POST',
+		headers: new Headers({ 'Content-Type': 'application/json'}),
+		body: JSON.stringify({ refresh })
+	});
+
+	if (res.status === 200) {
+		user.set({ accessKey: (await res.json()).access, refreshKey: refresh });
+		return true;
+	}
+
+	return false;
+}
+
+
+export const makeApiCall = async (url: string, config: RequestInit): Promise<Response> => {
+	let userVal = {
+		accessKey: '',
+		refreshKey: ''
+	};
+
+	user.subscribe((value) => {
+		userVal = value;
+	})();
+
+	if (!userVal.accessKey) {
+		await goto('/login');
+	}
+
+	const init = {
+		...config,
+		headers: new Headers({
+			Authorization: `Bearer ${userVal.accessKey}`,
+		})
+	};
+
+	const res = await fetch(`${PUBLIC_API_HOST}/${url}`, init);
+	if (res.status === 401) {
+		if (await refreshAccessToken(userVal.refreshKey)) {
+			return makeApiCall(url, config);
+		} else {
+			await goto('/login');
+		}
+	}
+
+	return res;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
 
 	import { user } from '$lib/stores';
+	import { makeApiCall } from '$lib/auth_service';
 
 	let userValue;
 	user.subscribe((value) => {
 		userValue = value;
 	});
+
+
+	const test = async () => {
+		console.log(await makeApiCall('api/profile', {}));
+	}
+
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 {userValue.accessKey}
+
+<button on:click={test}>Test</button>
