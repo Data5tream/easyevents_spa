34c3c56f37c5f0cf38121ffb75153c60e54aebab
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-08-18 17:01:44 +0200

    cleanup layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
+	import "carbon-components-svelte/css/g90.css";
+import {
+	Content,
+	Header,
+	SkipToContent
+} from 'carbon-components-svelte';
 	export const ssr = false;
 </script>
 
-<slot />
+<Header company='EasyEvents'>
+	<svelte:fragment slot='skip-to-content'>
+		<SkipToContent />
+	</svelte:fragment>
+</Header>
+
+<Content>
+	<slot />
+</Content>
