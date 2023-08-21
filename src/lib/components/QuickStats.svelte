2e9486a8b5f8f7dbae74148bef74550117d369bb
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-08-21 17:40:50 +0200

    improve dashboard styling and add quick link components

---
+++ src/lib/components/QuickStats.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Tile } from 'carbon-components-svelte';
+
+	const stats = [
+		{
+			title: 'Events',
+			count: 10
+		},
+		{
+			title: 'Participants',
+			count: 910
+		}
+	];
+</script>
+
+<Tile>
+	<h2>Quick Stats</h2>
+	<div class="stats-container">
+		{#each stats as stat}
+			<div class="info">
+				<span>{stat.count}</span>
+				<span>{stat.title}</span>
+			</div>
+		{/each}
+	</div>
+</Tile>
+
+<style>
+	.stats-container {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+	}
+
+	.info > span:first-child {
+		font-size: 5rem;
+	}
+</style>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,22 +1,55 @@
 <script lang="ts">
-
 	import { user } from '$lib/stores';
-	import { makeApiCall } from '$lib/auth_service';
+	import { Column, DataTable, Grid, Row, Tile } from 'carbon-components-svelte';
+	import QuickStats from '$lib/components/QuickStats.svelte';
+	import QuickLinks from '$lib/components/QuickLinks.svelte';
 
 	let userValue;
 	user.subscribe((value) => {
 		userValue = value;
 	});
-
-	let username = '';
-	const test = async () => {
-		const apiData = await (await makeApiCall('api/profile', {})).json();
-		username = apiData.user;
-	}
-
 </script>
 
-<h1>Dashboard</h1>
-<p>Your username: {username}</p>
+<Grid noGutter>
+	<Row>
+		<Column>
+			<h1>Dashboard</h1>
+		</Column>
+	</Row>
+	<Row>
+		<Column md={4}>
+			<QuickLinks />
+			<QuickStats />
+		</Column>
+		<Column md={4}>
+			<Tile>
+				<h2>Updates</h2>
+				<DataTable
+					headers={[
+						{ key: 'name', value: 'Name' },
+						{ key: 'event', value: 'Event' },
+						{ key: 'acc', value: 'Action' }
+					]}
+					rows={[
+						{ id: 1, name: 'Alice', event: 'Test Event', acc: 'joined' },
+						{ id: 2, name: 'Bob', event: 'Test Event', acc: 'joined' },
+						{ id: 3, name: 'Charlie', event: 'Test Event', acc: 'joined' },
+						{ id: 4, name: 'Bob', event: 'Test Event 2', acc: 'left' },
+						{ id: 5, name: 'Charlie', event: 'Test Event 3', acc: 'joined' },
+						{ id: 6, name: 'Alice', event: 'Test Event 4', acc: 'left' },
+						{ id: 7, name: 'Alice', event: 'Test Event 5', acc: 'joined' }
+					]}
+				/>
+			</Tile>
+		</Column>
+	</Row>
+</Grid>
 
-<button on:click={test}>Test</button>
+<style>
+	h1 {
+		margin-bottom: 2rem;
+	}
+	h2 {
+		margin-bottom: 1rem;
+	}
+</style>
