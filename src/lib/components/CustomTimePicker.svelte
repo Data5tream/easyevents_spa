1a6cbd6bac630da3f12927759aac6bfb2ddd8362
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-08-23 12:33:30 +0200

    add custom time picker component

---
+++ src/lib/components/CustomTimePicker.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { TimePicker } from 'carbon-components-svelte';
+
+  const timePattern = '[0-2]?\\d:[0-5]\\d';
+
+  const formatTime = (event: KeyboardEvent) => {
+    if (!event.target) {
+      return;
+    }
+
+    const ele = event.target as HTMLInputElement;
+    let val = ele.value;
+
+    if (val.length > 2 && val[2] !== ':') {
+      ele.value = `${val.substring(0, 2)}:${val.substring(2)}`;
+    }
+  };
+</script>
+
+<TimePicker {...$$props} on:keyup={formatTime} pattern={timePattern} />
