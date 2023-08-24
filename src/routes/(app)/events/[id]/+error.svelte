3c1ea761f625d0d7231b04910ad60e9e404a25a0
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-08-24 15:40:06 +0200

    improve error handling for event detail pages

---
+++ src/routes/(app)/events/[id]/+error.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Button } from 'carbon-components-svelte';
+</script>
+
+<div class="error-page">
+  <h1>404</h1>
+  <div>
+    <h2>No event found</h2>
+    <Button href="/events">Go to event list</Button>
+  </div>
+</div>
+
+<style>
+  .error-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 4rem;
+  }
+  h1 {
+    text-align: center;
+  }
+</style>
--- src/routes/(app)/events/[id]/+page.ts
+++ src/routes/(app)/events/[id]/+page.ts
@@ -1,8 +1,12 @@
 import { loadEvent } from '$lib/api_service';
 import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
-export const load: PageLoad = ({ params }) => {
-  const event = loadEvent(params.id);
+export const load: PageLoad = async ({ params }) => {
+  const event = await loadEvent(params.id);
+  if (!event) {
+    throw error(404, 'event not found');
+  }
 
   return {
     event
