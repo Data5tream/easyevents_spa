8e638a488f6f1e3da146e24fa6539bb70917320a
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-08-28 13:11:56 +0200

    Show signup link on event details page
    Also improve the layout of the detail page

--- src/routes/(app)/events/[id]/+page.svelte
+++ src/routes/(app)/events/[id]/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import EventReview from '$lib/components/EventReview.svelte';
-  import { Breadcrumb, BreadcrumbItem } from 'carbon-components-svelte';
+  import { Breadcrumb, BreadcrumbItem, Column, Grid, Row, Tile } from 'carbon-components-svelte';
   import EventParticipants from '$lib/components/EventParticipants.svelte';
+  import { PUBLIC_API_HOST } from '$env/static/public';
 
   export let data;
   const event = data.event;
   const participants = data.event.participants;
+  const event_link = `${PUBLIC_API_HOST}/signup/${event.id}/${encodeURIComponent(event.title)}`;
 </script>
 
 <Breadcrumb noTrailingSlash style="margin-bottom: 1rem">
@@ -14,14 +16,32 @@
   <BreadcrumbItem href="/events/{event.id}" isCurrentPage>{event.title}</BreadcrumbItem>
 </Breadcrumb>
 
-<EventReview
-  title={event.title}
-  description={event.description}
-  maxParticipants={event.max_participants}
-  requireConfirmation={event.require_confirmation}
-  eventStart={new Date(Date.parse(event.start_date))}
-  eventEnd={new Date(Date.parse(event.end_date))}
-  signupStart={new Date(Date.parse(event.signup_start))}
-  signupEnd={new Date(Date.parse(event.signup_end))}
-/>
-<EventParticipants {participants} max_participants={event.max_participants} />
+<Grid noGutter>
+  <Row>
+    <Column>
+      <EventReview
+        title={event.title}
+        description={event.description}
+        maxParticipants={event.max_participants}
+        requireConfirmation={event.require_confirmation}
+        eventStart={new Date(Date.parse(event.start_date))}
+        eventEnd={new Date(Date.parse(event.end_date))}
+        signupStart={new Date(Date.parse(event.signup_start))}
+        signupEnd={new Date(Date.parse(event.signup_end))}
+      />
+    </Column>
+  </Row>
+  <Row>
+    <Column>
+      <Tile>
+        <h3>Invite link</h3>
+        <p><a href={event_link} target="_blank" rel="noreferrer">{event_link}</a></p>
+      </Tile>
+    </Column>
+  </Row>
+  <Row>
+    <Column>
+      <EventParticipants {participants} max_participants={event.max_participants} />
+    </Column>
+  </Row>
+</Grid>
