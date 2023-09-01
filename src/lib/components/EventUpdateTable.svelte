47c5a8f52c4c31fb1c7d6dd34be422198bee11b2
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-09-01 15:11:35 +0200

    add pagination to event updates

--- src/lib/components/EventUpdateTable.svelte
+++ src/lib/components/EventUpdateTable.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { DataTable, Tile } from 'carbon-components-svelte';
+  import { DataTable, Pagination, Tile } from 'carbon-components-svelte';
   import type { EventUpdate } from '$lib/api_service.js';
 
   export let ev_updates: Array<EventUpdate>;
@@ -11,39 +11,45 @@
     event_type: u.event_type,
     timestamp: new Date(u.timestamp).toLocaleString()
   }));
+
+  let pageSize = 10;
+  let page = 1;
 </script>
 
 {#if updates.length}
-<DataTable
-  title="Event Updates"
-  description="User updates related to this event"
-  headers={[
-    { key: 'name', value: 'Name' },
-    { key: 'event_type', value: 'Action' },
-    { key: 'timestamp', value: 'Timestamp' }
-  ]}
-  rows={updates}
->
-  <svelte:fragment slot="cell" let:cell>
-    {#if cell.key === 'event_type'}
-      {#if cell.value === 'joined'}
-        <span class="success">Joined</span>
-      {:else if cell.value === 'kicked'}
-        <span class="danger">Kicked</span>
-      {:else if cell.value === 'left'}
-        <span class="danger">Left</span>
+  <DataTable
+    title="Event Updates"
+    description="User updates related to this event"
+    headers={[
+      { key: 'name', value: 'Name' },
+      { key: 'event_type', value: 'Action' },
+      { key: 'timestamp', value: 'Timestamp' }
+    ]}
+    rows={updates}
+    {pageSize}
+    {page}
+  >
+    <svelte:fragment slot="cell" let:cell>
+      {#if cell.key === 'event_type'}
+        {#if cell.value === 'joined'}
+          <span class="success">Joined</span>
+        {:else if cell.value === 'kicked'}
+          <span class="danger">Kicked</span>
+        {:else if cell.value === 'left'}
+          <span class="danger">Left</span>
+        {:else}
+          {cell.value}
+        {/if}
       {:else}
         {cell.value}
       {/if}
-    {:else}
-      {cell.value}
-    {/if}
-  </svelte:fragment>
-</DataTable>
+    </svelte:fragment>
+  </DataTable>
+  <Pagination bind:pageSize bind:page totalItems={updates.length} />
 {:else}
   <Tile>
     <h4>No updates yet</h4>
-    <p class='bx--data-table-header__description'>
+    <p class="bx--data-table-header__description">
       There are no updates for this event yet. Once users join the event, a table will appear here
     </p>
   </Tile>
